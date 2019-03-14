dc7525bd7aeb7fdb1c69a5b4a2a60bd8cadec3fd
Puneet Patwari <puneet.patwari@thoughtworks.com>
2019-03-14 18:54:51 +0530

    #DEOPSCSW-645: Puneet : Disable module handling in filebeat and restore oss version of elastic stack

--- elasticsearch/configure.sh
+++ elasticsearch/configure.sh
@@ -2,4 +2,4 @@
 
 # wait for Elasticsearch to start, then run the setup script to
 # create and configure the index.
-filebeat -c filebeat-init.yml -v -e setup --pipelines --modules system,postgresql
+filebeat -c filebeat-init.yml -v -e setup --pipelines --modules system,postgresql,redis
