aff4f4e9dfebf5a6175b7056c4b6531d4058fd18
Puneet Patwari <puneet.patwari@thoughtworks.com>
2019-03-14 12:25:59 +0530

    ##DEOPSCSW-641: Puneet: Add filebeat system module

--- elasticsearch/configure.sh
+++ elasticsearch/configure.sh
@@ -2,15 +2,4 @@
 
 # wait for Elasticsearch to start, then run the setup script to
 # create and configure the index.
-curl -s -H 'Content-Type: application/json' -XPUT "http://elasticsearch:9200/_ingest/pipeline/test-pipeline" -d '{
-                  "description": "Test pipeline",
-                  "processors": [
-                      {
-                          "json": {
-                              "if": "ctx.message.startsWith(\"{\")",
-                              "field": "message",
-                              "target_field": "tmt"
-                          }
-                      }
-                  ]
-              }'
+filebeat -c filebeat-init.yml -v -e setup --pipelines --modules system,postgresql
